cf91bfdf11b1fa07c86fa6da0aecf540d558a17c
Isaac Luo <yishaluo@gmail.com>
2019-03-20 23:45:33 +0000

    add pictures into listbox

--- CVTester/MainWindow.xaml.cs
+++ CVTester/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace CVTester
 {
@@ -23,11 +24,33 @@ namespace CVTester
         public MainWindow()
         {
             InitializeComponent();
+            this.listBoxFileNames.Items.Add("1");
+            this.listBoxFileNames.Items.Add("2");
         }
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                this.fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                foreach(string fileName in this.fileNames)
+                {
+                    string mainFileName = System.IO.Path.GetFileNameWithoutExtension(fileName);
+                    this.listBoxFileNames.Items.Add(mainFileName);
+                }
+            }
+        }
 
+        private string[] fileNames;
+
+
+        private void ListBoxFileNames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            this.listBoxFileNames.Items.Add(e.AddedItems[0]);
         }
     }
 }
